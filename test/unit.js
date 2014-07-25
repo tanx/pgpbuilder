c660bc9c4ad1471ee11917504179983b6bfebaee
Felix Hammerl <mail@felixhammerl.com>
2014-07-25 16:34:00 +0200

    use npm deps

--- test/unit.js
+++ test/unit.js
@@ -5,13 +5,13 @@ require.config({
         'chai': '../node_modules/chai/chai',
         'sinon': '../node_modules/sinon/pkg/sinon',
         'mailbuild': '../node_modules/mailbuild/src/mailbuild',
-        'addressparser': '../node_modules/mailbuild/node_modules/addressparser/src/addressparser',
+        'wo-addressparser': '../node_modules/mailbuild/node_modules/wo-addressparser/src/addressparser',
         'mimefuncs': '../node_modules/mailbuild/node_modules/mimefuncs/src/mimefuncs',
         'mimetypes': '../node_modules/mailbuild/node_modules/mimetypes/src/mimetypes',
         'punycode': '../node_modules/mailbuild/node_modules/punycode/punycode',
-        'stringencoding': '../node_modules/mailbuild/node_modules/mimefuncs/node_modules/stringencoding/dist/stringencoding',
+        'wo-stringencoding': '../node_modules/mailbuild/node_modules/mimefuncs/node_modules/wo-stringencoding/dist/stringencoding',
         'openpgp': 'lib/openpgp.min',
-        'crypto': 'lib/dummy' // this is due to the fact how requirejs parses modules (there is a require(crypto) in the function)
+        'crypto': 'lib/dummy' // this is due to the fact how requirejs parses modules as text, even though it may never reach the require (there is a require(crypto) in the function)
     },
     shim: {
         'sinon': {
