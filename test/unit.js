81a1b7923a6b06b0359e5e388007a006dc81f264
Felix Hammerl <mail@felixhammerl.com>
2014-07-28 16:07:38 +0200

    fix npm deps

--- test/unit.js
+++ test/unit.js
@@ -5,11 +5,11 @@ require.config({
         'chai': '../node_modules/chai/chai',
         'sinon': '../node_modules/sinon/pkg/sinon',
         'mailbuild': '../node_modules/mailbuild/src/mailbuild',
-        'wo-addressparser': '../node_modules/mailbuild/node_modules/wo-addressparser/src/addressparser',
+        'addressparser': '../node_modules/mailbuild/node_modules/wo-addressparser/src/addressparser',
         'mimefuncs': '../node_modules/mailbuild/node_modules/mimefuncs/src/mimefuncs',
         'mimetypes': '../node_modules/mailbuild/node_modules/mimetypes/src/mimetypes',
         'punycode': '../node_modules/mailbuild/node_modules/punycode/punycode',
-        'wo-stringencoding': '../node_modules/mailbuild/node_modules/mimefuncs/node_modules/wo-stringencoding/dist/stringencoding',
+        'stringencoding': '../node_modules/mailbuild/node_modules/mimefuncs/node_modules/wo-stringencoding/dist/stringencoding',
         'openpgp': 'lib/openpgp.min',
         'crypto': 'lib/dummy' // this is due to the fact how requirejs parses modules as text, even though it may never reach the require (there is a require(crypto) in the function)
     },
